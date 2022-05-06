2620c9c344bbcdea9b729417f607f58bb7da3fdc
belbix <7453635@gmail.com>
2022-05-06 17:20:41 +0300

    fix WMATIC interface

--- contracts/test/Token.sol
+++ contracts/test/Token.sol
@@ -166,43 +166,13 @@ contract Token {
 
   // --------------------- WMATIC
 
-  // Error Code: No error.
-  uint256 public constant ERR_NO_ERROR = 0x0;
-
-  // Error Code: Non-zero value expected to perform the function.
-  uint256 public constant ERR_INVALID_ZERO_VALUE = 0x01;
-
-  // deposit wraps received FTM tokens as wFTM in 1:1 ratio by minting
-  // the received amount of FTMs in wFTM on the sender's address.
-  function deposit() public payable returns (uint256) {
-    // there has to be some value to be converted
-    if (msg.value == 0) {
-      return ERR_INVALID_ZERO_VALUE;
-    }
-
-    // we already received FTMs, mint the appropriate amount of wFTM
-    _mint(msg.sender, msg.value);
-
-    // all went well here
-    return ERR_NO_ERROR;
+  function deposit() public payable {
+    balanceOf[msg.sender] += msg.value;
   }
 
-  // withdraw unwraps FTM tokens by burning specified amount
-  // of wFTM from the caller address and sending the same amount
-  // of FTMs back in exchange.
-  function withdraw(uint256 amount) public returns (uint256) {
-    // there has to be some value to be converted
-    if (amount == 0) {
-      return ERR_INVALID_ZERO_VALUE;
-    }
-
-    // burn wFTM from the sender first to prevent re-entrance issue
-    burn(msg.sender, amount);
-
-    // if wFTM were burned, transfer native tokens back to the sender
-    payable(msg.sender).transfer(amount);
-
-    // all went well here
-    return ERR_NO_ERROR;
+  function withdraw(uint wad) public {
+    require(balanceOf[msg.sender] >= wad);
+    balanceOf[msg.sender] -= wad;
+    payable(msg.sender).transfer(wad);
   }
 }
--- scripts/addresses/MaticAddresses.ts
+++ scripts/addresses/MaticAddresses.ts
@@ -8,5 +8,11 @@ export class MaticAddresses {
 
   // tokens
   public static WMATIC_TOKEN = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270".toLowerCase();
+  public static WETH_TOKEN = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619".toLowerCase();
+  public static USDC_TOKEN = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174".toLowerCase();
+  public static WBTC_TOKEN = "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6".toLowerCase();
+  public static FRAX_TOKEN = "0x104592a158490a9228070E0A8e5343B499e125D0".toLowerCase();
+  public static DAI_TOKEN = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063".toLowerCase();
+  public static USDT_TOKEN = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F".toLowerCase();
 
 }
